a45f69353aba775b421215615c059c7447d516d7
nelitovec <msfelzhel@gmail.com>
2024-10-18 23:19:36 +0300

    фикс

--- 25063987/12.py
+++ 25063987/12.py
@@ -6,7 +6,7 @@ def getSum(x):
 n = '3' * 111
 while n.count('33333') >= 1 or n.count('1111',1) >= 1:
     if n.count('33333') >= 1:
-        n = n.replace('3333','111',1)
+        n = n.replace('33333','111',1)
     else:
         n = n.replace('111','33',1)
 print(getSum(n))
--- 25063987/16.py
+++ 25063987/16.py
@@ -1,8 +1,8 @@
 from sys import * 
-setrecursionlimit(10000)
+setrecursionlimit(5000)
 
 
 def f(n):
     if n < 5: return 4**4 
     if n > 4: return 4*f(n-4) + 4
-print(f(4048/f(4036))) 
+print(f(4048)/f(4036)) 
